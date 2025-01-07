76f05a203f364cd54c45fbc14a7648519e5099b6
Your Name <you@example.com>
2025-01-07 15:02:45 +0600

    dd

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
         <h1 className="text-4xl sm:text-6xl font-extrabold text-gray-800 mb-4">404</h1>
         <p className="text-lg sm:text-2xl text-gray-700 mb-4">Oops! The page you're looking for does not exist.</p>
         <p className="text-base sm:text-lg text-gray-600 mb-8">It seems like the page has been moved or deleted.</p>
-        <Link to="/" className="bg-yellow-400 text-gray-800 py-2 px-6 rounded-full text-sm sm:text-lg font-semibold hover:bg-yellow-400 transition duration-300">
+        <Link to="/" className="bg-yellow-500 text-gray-800 py-2 px-6 rounded-full text-sm sm:text-lg font-semibold hover:bg-yellow-400 transition duration-300">
           Go Back Home
         </Link>
       </div>
