bc48a3f6df252ac85c1e5b07efd4c4b36e47d5c3
Your Name <you@example.com>
2025-01-12 15:37:24 +0600

    chainge some data and code

--- src/serviceData.js
+++ src/serviceData.js
@@ -1,9 +1,10 @@
 import image1 from '../src/assets/pressere_test.jpeg';
-import image2 from '../src/assets/our-service-1 (2).jpg';
+import image2 from '../src/assets/ser-1.webp';
+import image3 from '../src/assets/ser-2.webp';
 
 const data = [
     {
-        image: "https://files.oaiusercontent.com/file-9U9WPkfeRaJhw6rDkR55Eo?se=2025-01-12T08%3A12%3A32Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Dd6f7f37c-1627-4569-9a81-00b80f87c517.webp&sig=CXOeIR29nG8MkSyMXtFJtM4QI7HpnHJQHC3nuOOkg34%3D",
+        image: image3,
         name: "Lifting Equipment Inspection",
         proposition: "Ensuring your lifting equipment meets safety and operational standards through comprehensive inspections.",
         id: 1
@@ -15,7 +16,7 @@ const data = [
         id: 2
     },
     {
-        image: "https://files.oaiusercontent.com/file-7eBDV63NyRTXVEDiaiUXah?se=2025-01-12T08%3A09%3A51Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Db2c93fec-bab2-4d01-bf44-de6d0ee4886b.webp&sig=43ugyV3nxN9fxp5WRCuiovE5t%2BmGscKJ9GpDfns6khs%3D",
+        image: image2,
         name: "NDT Test",
         proposition: "Non-destructive testing to identify potential flaws without causing damage to your equipment.",
         id: 3
